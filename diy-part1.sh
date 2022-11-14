42c470ec6653f8b79e709915b9199b988a02921d
child <wulincheng02@outlook.com>
2022-11-14 15:12:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git kenzok8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git fw876 https://github.com/fw876/helloworld' >>feeds.conf.default
