7970dae560f0a4fc50126c46c68c45e649d511d8
child <wulincheng02@outlook.com>
2022-11-14 17:02:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@
 #============================================================
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.99.101/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把XPK修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt'' package/lean/default-settings/files/zzz-default-settings
