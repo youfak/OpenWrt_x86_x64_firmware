d12ffe06bdef9e3d0c3ab5714be239767a52ab36
child <wulincheng02@outlook.com>
2022-11-14 20:53:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-echo  sed -i 'src-git small https://github.com/kenzok8/small' feeds.conf.default
-echo  'src-git fw876 https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' feeds.conf.default
+echo 'src-git fw876 https://github.com/fw876/helloworld' >>feeds.conf.default
