c268acfa0c77d0b700b77e0ce22f13dcea91a785
child <wulincheng02@outlook.com>
2022-11-19 12:35:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -30,6 +30,7 @@ rm -rf ./feeds/luci-theme-neobird
 
 # 删除自带的adguardhome
 rm -rf ./feeds/packages/net/adguardhome
+rm -rf package/luci-app-adguardhome
 
 #禁止Turbo ACC 网络加速修改net.bridge.bridge-nf-call-iptables的值为1(修改为1后旁路由需开启ip动态伪装，影响下行带宽)。
 #sed -i '/exit 0/i sed -i "s/\\[ -d \\/sys\\/kernel\\/debug\\/ecm\\/ecm_nss_ipv4 \\] \\&\\& return 0/\\[ -d \\/sys\\/kernel\\/debug\\/ecm\\/ecm_nss_ipv4 \\] \\&\\& return 1/g" /etc/init.d/qca-nss-ecm'  package/lean/default-settings/files/zzz-default-settings
@@ -42,6 +43,10 @@ git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-th
 git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
 git clone -b 21.02 https://github.com/kenzok78/luci-theme-argonne package/luci-theme-argonne
 git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
+
+# 添加指定 adguardhome
+svn co https://github.com/kiddin9/openwrt-packages/tree/master/adguardhome  feeds/packages/net/adguardhome
+svn co https://github.com/kiddin9/openwrt-packages/tree/master/luci-app-adguardhome package/luci-app-adguardhome
 
 # Modify hostname
 #sed -i 's/OpenWrt/OpenWrt/g' package/base-files/files/bin/config_generate
