86def07c6949444611fd687d1375d8c1281b898e
wulincheng <wulincheng@javalc.com>
2023-02-24 21:22:35 +0800

    添加自定义openclash源

--- diy5.10.sh
+++ diy5.10.sh
@@ -13,6 +13,9 @@
 # 指定编译版本
 sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 
+# 使用自定义openclash
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
--- diy6.0.sh
+++ diy6.0.sh
@@ -14,6 +14,9 @@
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.0/g' ./target/linux/x86/Makefile
 sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=6.0/g' ./target/linux/x86/Makefile
 
+# 使用自定义openclash
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
