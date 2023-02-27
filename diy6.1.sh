ca4d1ca74c4c3f31f685a64cb93ad5d890f8f3a6
wulincheng <wulincheng@javalc.com>
2023-02-27 17:36:02 +0800

    更新固件6.1

--- diy6.1.sh
+++ diy6.1.sh
@@ -11,8 +11,8 @@
 #
 
 # 切换固件版本
-sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.0/g' ./target/linux/x86/Makefile
-sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=6.0/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=6.1/g' ./target/linux/x86/Makefile
 
 # 使用自定义openclash
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
