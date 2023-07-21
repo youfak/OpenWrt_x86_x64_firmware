dcd988e35d0c04cf0bab0523abdaafa7f96fc44e
child008 <wulincheng02@outlook.com>
2023-07-21 09:58:57 +0800

    Update diy5.15.sh

--- diy5.15.sh
+++ diy5.15.sh
@@ -12,6 +12,7 @@
 #
 # 指定编译版本
 sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
 
 # 使用自定义openclash
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
