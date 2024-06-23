94fa7f39646945cf843170b340edae1d2d5a6f0f
Bad Child <wulincheng02@outlook.com>
2024-06-23 13:39:32 +0800

    Update diy5.10.sh

--- diy5.10.sh
+++ diy5.10.sh
@@ -22,9 +22,10 @@ sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Make
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small8 https://github.com/kenzok8/small-package'  feeds.conf.default
-# sed -i '$a src-git fw876 https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git small8 https://github.com/kenzok8/small-package'  feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git fw876 https://github.com/fw876/helloworld' feeds.conf.default
 # sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 
 # 使用自定义主题
